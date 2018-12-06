2f698bf81a51ef53cca40792ae1c0d1385cf1e75
heyitschrislol <heyitschrislol@users.noreply.github.com>
2018-12-06 11:46:42 -0600

    made it prettier for mobile

--- js/ch_display.js
+++ js/ch_display.js
@@ -1,9 +1,12 @@
+
 function xopen() {
     document.getElementById("mySidebar").style.display = "block";
     document.getElementById("myOverlay").style.display = "block";
+    document.getElementById("openNav").style.display = "none";
 }
  
 function xclose() {
     document.getElementById("mySidebar").style.display = "none";
     document.getElementById("myOverlay").style.display = "none";
+    document.getElementById("openNav").style.display = "block";
 }
