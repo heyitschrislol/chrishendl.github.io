0cd92eb68094be97fe3bdec10edceb6b4ce7bcd7
heyitschrislol <heyitschrislol@users.noreply.github.com>
2018-12-06 10:35:40 -0600

    attempt to add mobile dropdown nav

---
+++ js/ch_display.js
@@ -0,0 +1,9 @@
+function xopen() {
+    document.getElementById("mySidebar").style.display = "block";
+    document.getElementById("myOverlay").style.display = "block";
+}
+ 
+function xclose() {
+    document.getElementById("mySidebar").style.display = "none";
+    document.getElementById("myOverlay").style.display = "none";
+}
