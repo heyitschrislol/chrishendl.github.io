0bf2fa94dd8b1f52e1b45df7afd617dd4818518a
Chris Hendl <Christopher.Hendl@ibm.com>
2019-12-13 09:11:59 -0600

    found the prob

--- js/ch_gallery.js
+++ js/ch_gallery.js
@@ -9,17 +9,17 @@
 // }
 function pbImage(imgs) {
 	// Get the expanded image
-	if (window.innerWidth < 900) {
+	// if (window.innerWidth > 900) {
 
 	var expandImg = document.getElementById("pb-image");
 	expandImg.src = imgs.src;
 	// Show the container element (hidden with CSS)
 	expandImg.style.display = "block";
 	document.getElementById("pb-close").style.display = "block";
-	}
+	// }
 }
 function mmImage(imgs) {
-	if (window.innerWidth < 900) {
+	// if (window.innerWidth > 900) {
 
 	// Get the expanded image
 	var expandImg = document.getElementById("mm-image");
@@ -27,10 +27,10 @@ function mmImage(imgs) {
 	// Show the container element (hidden with CSS)
 	expandImg.style.display = "block";
 	document.getElementById("mm-close").style.display = "block";
-	}
+	// }
 }
 function dcImage(imgs) {
-	if (window.innerWidth < 900) {
+	// if (window.innerWidth < 900) {
 
 	// Get the expanded image
 	var expandImg = document.getElementById("dc-image");
@@ -38,10 +38,10 @@ function dcImage(imgs) {
 	// Show the container element (hidden with CSS)
 	expandImg.style.display = "block";
 	document.getElementById("dc-close").style.display = "block";
-	}
+	// }
 }
 function seImage(imgs) {
-	if (window.innerWidth < 900) {
+	// if (window.innerWidth < 900) {
 
 	// Get the expanded image
 	var expandImg = document.getElementById("se-image");
@@ -49,10 +49,10 @@ function seImage(imgs) {
 	// Show the container element (hidden with CSS)
 	expandImg.style.display = "block";
 	document.getElementById("se-close").style.display = "block";
-	}
+	// }
 }
 function rtImage(imgs) {
-	if (window.innerWidth < 900) {
+	// if (window.innerWidth < 900) {
 
 	// Get the expanded image
 	var expandImg = document.getElementById("rt-image");
@@ -60,7 +60,7 @@ function rtImage(imgs) {
 	// Show the container element (hidden with CSS)
 	expandImg.style.display = "block";
 	document.getElementById("rt-close").style.display = "block";
-	}
+	// }
 }
 function closeImages() {
 	// document.getElementById("mobile-ex-gallery").style.display = "none";
