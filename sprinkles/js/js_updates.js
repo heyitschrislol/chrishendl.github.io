f465dc3b9cf55252adc82e414e4c1c3acff0c4f4
heyitschrislol <heyitschrislol@users.noreply.github.com>
2020-07-13 13:52:20 -0500

    sprinkles test 2

--- sprinkles/js/js_updates.js
+++ sprinkles/js/js_updates.js
@@ -7,14 +7,17 @@ function featuredUpdate() {
 			document.getElementById("featured-txt-a").innerHTML = myArr[0].type;
 			document.getElementById("featured-txt-b").innerHTML = myArr[0].title;
 			document.getElementById("featured-txt-c").innerHTML = myArr[0].date;
+			document.getElementById("featured-img").src = myArr[0].image;
 			document.getElementById("featured-txt-1a").innerHTML = myArr[1].type;
 			document.getElementById("featured-txt-1b").innerHTML = myArr[1].title;
 			document.getElementById("featured-txt-1c").innerHTML = myArr[1].date;
+			document.getElementById("sub-featured-img-a").src = myArr[1].image;
 			document.getElementById("featured-txt-2a").innerHTML = myArr[2].type;
 			document.getElementById("featured-txt-2b").innerHTML = myArr[2].title;
 			document.getElementById("featured-txt-2c").innerHTML = myArr[2].date;
+			document.getElementById("sub-featured-img-b").src = myArr[2].image;
 		}
 	};
-	xmlhttp.open("GET", "sprinkles/js/updates.txt", true);
+	xmlhttp.open("GET", "js/updates.txt", true);
 	xmlhttp.send();
 }
