0165d57d25f56dea7406e2cf717ab5cecfc1cf8e
heyitschrislol <heyitschrislol@users.noreply.github.com>
2021-07-18 12:08:19 -0500

    vhs viewport

--- js/cinema.js
+++ js/cinema.js
@@ -53,25 +53,9 @@ function burgerTime() {
         document.getElementsByClassName("hamburger").style.height = "8px";
     }
 }
-// function getMovieList() {
-//     let requestURL = 'https://github.com/heyitschrislol/chrishendl.github.io/blob/master/js/movielist.json';
-//     let request = new XMLHttpRequest();
-//     request.open('GET', requestURL);
-//     request.responseType = 'json';
-//     request.send();
-//     request.onload = function() {
-//         const movielist = request.response;
-
-//     }
-
-// }
 
 
-// function poopee() {
-//     const list = '[{"title":"Baby Gender Reveal","url":"https://www.youtube.com/embed/JN2iGKOGQFM"},{"title":"Chris and Jennifer Swimming","url":"https://www.youtube.com/embed/zTd71mMaaGQ"}]';
-//     movies = JSON.parse(list);
-//     // alert(movies[1].url);
-//     document.getElementById("currentvid").setAttribute("src", movies[0].url);
-//     document.getElementById("currentvid").load();
-//     document.getElementById("movietitle").innerText = movies[0].title;
-// }
+function animateLogo() {
+    var logo = document.getElementById("logo");
+    
+}
